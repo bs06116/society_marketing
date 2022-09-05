cf56a9e479ff9b7d47fc91778def8b933bc6663c
Danish Ayoub <danishinreallife@gmail.com>
2022-09-06 01:19:43 +0500

    pdf design

--- resources/views/pages/pdf.blade.php
+++ resources/views/pages/pdf.blade.php
@@ -1,48 +1,58 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<style>
-    /* .dataTables_length{
-        display:none;
-    }
-    .dataTables_info{
-        display:none;
-    }
-    .dataTables_paginate{
-        display:none;
-    } */
-</style>
-<div style="max-width: 90mm; margin: 50px auto 0 auto;">
-    <div class="row mb-3">
-        <div class="col-6">
-            <address>
-                <strong>Receipt</strong><br>
-            </address>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> -->
+        <style>
+            body {
+                color: #444;
+                font-family: "Poppins", sans-serif;
+            }
+            .heading {
+                text-align: center;
+                font-size: 20px;
+                margin: 0 0 20px;
+            }
+            .golden-heading {
+                padding: 7px;
+                border-radius: 10px;
+                font-size: 16px;
+                color: white;
+                background-color: #e2a324;
+                margin: 0 auto 50px;
+                text-align: center;
+                width: 230px;
+            }
+            .float-right {
+                float: right;
+            }
+            .float-left {
+                float: left;
+            }
+            .clear-both {
+                clear: both;
+            }
+            .text {
+                font-size: 15px;
+                margin: 0 0 10px 0;
+                padding-right: 5px;
+            }
+            .text span {
+                font-weight: 400;
+            }
+        </style>
+    </head>
+    <body>
+        <h2 class="heading">CASH RECEIPT</h2>
+        <h3 class="golden-heading">ACCOUNT DEPARTMENT</h3>
+        <div>
+            <div class="float-left">
+                <h2 class="text">Date:<span>9/6/2022</span></h2>
+            </div>
+            <div class="float-right">
+                <h2 class="text">Receipt No.<span>125</span></h2>
+            </div>
         </div>
-        <div class="col-6 text-end">
-            <p class="mb-2">
-                <em>Date: {{date('m/d/Y')}}</em>
-            </p>
-            <p>
-                <em>Receipt #: {{time()}}</em>
-            </p>
-        </div>
-    </div>
-    <div class="row">
-        <table class="table table-hover" id="invoice_table">
-            <thead>
-                <tr>
-                    <th>Customer</th>
-                    <th>Currency</th>
-                    <th>Rate</th>
-                    <th>Paid</th>
-                    <th>Received</th>
-                    <th>Is Daily Transation</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-            <tfoot>
-            </tfoot>
-        </table>
-    </div>
-</div>
+    </body>
+</html>
